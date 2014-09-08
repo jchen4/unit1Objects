88baa1e06ad0a0190dc616d216f35956b9dde81d
jchen4 <jchen4@student.naperville203.org>
2014-09-08 10:56:56 -0500

    turtlelab
    turtles

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class TurtleWorld
 {
@@ -6,8 +7,44 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        Turtle turtle2 = new Turtle(turtleWorld);
+        turtle.turnLeft();
+        turtle2.turnRight();
         turtle.penDown();
-        turtle.forward(50);
+        turtle2.penDown();
+        turtle.setPenColor(Color.RED);
+        turtle2.setPenColor(Color.GREEN);
+        int ctr = 1;
+        int ctr2 = 1;
+        int r = 180;
+        while (r > 0)
+        {
+            turtle.forward(360/r);
+            turtle2.forward(360/r);
+            turtle2.turn(r);
+            turtle.turn(r);
+            ctr = ctr + r;
+            r = r-1;
+        }
+        turtle.penUp();
+        turtle2.penUp();
+        turtle.moveTo(320,240);
+        turtle2.moveTo(320,240);
+        turtle.setPenColor(Color.YELLOW);
+        turtle2.setPenColor(Color.BLUE);
+        turtle.penDown();
+        turtle2.penDown();
+        ctr = 1;
+        ctr2 = 1;
+        r = 180;
+        while (r > 0)
+        {
+            turtle.forward(360/r);
+            turtle2.forward(360/r);
+            turtle2.turn(r);
+            turtle.turn(r);
+            ctr = ctr + r;
+            r = r-1;
+        }
     }
 }
