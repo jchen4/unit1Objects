c3520903f5c85c8de284990306312a439c27277d
jchen4 <jchen4@student.naperville203.org>
2014-09-09 10:50:29 -0500

    turtlelabnewsync
    yeah

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,17 +3,19 @@ import java.util.Random;
 
 public class TurtleWorld
 {
-    public static void main(String args[])
+    public static void main(String args[])throws InterruptedException
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
         Turtle turtle2 = new Turtle(turtleWorld);
+        turtle.setShellColor(Color.BLUE);
+        turtle2.setShellColor(Color.RED);
         turtle.turnLeft();
         turtle2.turnRight();
         turtle.penDown();
         turtle2.penDown();
-        turtle.setPenColor(Color.RED);
-        turtle2.setPenColor(Color.GREEN);
+        turtle.setPenColor(Color.BLACK);
+        turtle2.setPenColor(Color.BLACK);
         int ctr = 1;
         int ctr2 = 1;
         int r = 180;
@@ -25,13 +27,12 @@ public class TurtleWorld
             turtle.turn(r);
             ctr = ctr + r;
             r = r-1;
+            Thread.sleep(100);
         }
         turtle.penUp();
         turtle2.penUp();
         turtle.moveTo(320,240);
         turtle2.moveTo(320,240);
-        turtle.setPenColor(Color.YELLOW);
-        turtle2.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle2.penDown();
         ctr = 1;
@@ -45,6 +46,19 @@ public class TurtleWorld
             turtle.turn(r);
             ctr = ctr + r;
             r = r-1;
+            Thread.sleep(100);
+        }
+        turtle.penUp();
+        turtle2.penUp();
+        turtle.moveTo(320,240);
+        turtle2.moveTo(320,240);
+        while (r != 283284)
+        {
+          turtle.turn(10);
+          turtle2.turn(10);
+          Thread.sleep(100);
+          turtle.forward(36);
+          turtle2.forward(36);
         }
     }
 }
